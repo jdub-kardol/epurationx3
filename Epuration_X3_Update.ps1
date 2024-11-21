aa63742fae7e1ae1a743dd23b46bab2da413bdc1
jdub-kardol <jguy.dubois@kardol.fr>
2024-11-21 14:50:59 +0100

    Update Epuration_X3_Update.ps1

--- Epuration_X3_Update.ps1
+++ Epuration_X3_Update.ps1
@@ -1,2 +1,8 @@
 # MAJ 1.0
 write-output "script MAJ"
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+write-output "pas admin"
+}
+Else {
+write-output "ADMIN OK"
+}
